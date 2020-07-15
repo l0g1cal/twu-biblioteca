2f8428ff8dd41c519b0c2797d98a96799d7558b7
Jia Wern <yongjiawern@gmail.com>
2020-07-15 17:43:46 +0800

    Add welcome message

---
+++ src/main/webapp/js/constants.js
@@ -0,0 +1,2 @@
+export const welcomeMessage =
+  "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
---
+++ src/main/webapp/js/welcome.js
@@ -0,0 +1,7 @@
+import * as consts from "./constants.js";
+
+export default function showWelcomeMessage() {
+  window.alert(consts.welcomeMessage);
+}
+
+showWelcomeMessage();
